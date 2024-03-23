a1ca160fb1eed661e05bef8dc2f0f3900674627b
Justin Randall <justin@outofservice.org>
2024-03-23 16:01:15 -0400

    Improved documentation and added more of a "Hello, World!" type implementation.

--- src/routes/core.ts
+++ src/routes/core.ts
@@ -27,9 +27,11 @@ import { TagRouter, WebhookContext, WebhookRouter } from "@sitinc/dialogflowcx-t
 
 const router = new WebhookRouter();
 
-// Example tag route for 'New Interaction' tag.
+// Repeat the below create/register pattern for every new webhook "tag" handler.
+
+// Create the example Dialogflow CX "tag" router for the 'New' webhook tag.
 const newInteractionRoute = new class implements TagRouter {
-    public tagName: string = 'New Interaction'; // Dialogflow CX Tag value.
+    public tagName: string = 'New'; // Dialogflow CX Tag value.
 
     /**
      * Handle the webhook request.
@@ -40,14 +42,26 @@ const newInteractionRoute = new class implements TagRouter {
         const sessionId = crypto.randomUUID();
         context.params.sessionId = sessionId;
         
-        // Access the root of Dialoflow CX Parameters.
-        const sessionParams = context.req.body.sessionInfo.parameters;
+        // Root of Dialoflow CX Webhook Request Data.
+        const requestBody = context.req.body.sessionInfo.parameters;
+
+        // Root of Dialoflow CX Context Parameters.
+        const sessionParams = requestBody.sessionInfo.parameters;
 
         // TODO: The needful.
+        /*
+         * Here's a trivial example of read and updating Dialogflow context
+         * parameters.
+         */
+        const name = sessionParams.api_name || 'World';
+
+        sessionParams.api_session_id = sessionId
+        sessionParams.api_msg = `Hello, ${name}!`
 
         return context;
     }
 }
+// Register the tag router for webhook tag 'New'.
 router.use(newInteractionRoute.tagName, newInteractionRoute);
 
 export = router;
